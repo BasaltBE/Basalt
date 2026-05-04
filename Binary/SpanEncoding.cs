ecd0354ff59cb8498c025bf7975fad1551cf0d91
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-04 18:02:52 +0300

    add floats

--- Binary/SpanEncoding.cs
+++ Binary/SpanEncoding.cs
@@ -3,7 +3,8 @@ using System.Runtime.InteropServices;
 
 namespace Basalt.Binary
 {
-    public static partial class SpanEncodingExtensions {
+    public static partial class SpanEncodingExtensions
+    {
 #if BIGENDIAN
         public const bool IsLittleEndian = false;
 #else
@@ -19,6 +20,27 @@ namespace Basalt.Binary
             public void WriteUInt8(byte value, int offset = 0)
             {
                 source[offset] = value;
+            }
+
+            public void WriteBool(bool value, int offset = 0)
+            {
+                source[offset] = value ? (byte)1 : (byte)0;
+            }
+
+            public void WriteUInt24(uint value, int offset = 0, bool littleEndian = true)
+            {
+                if (littleEndian)
+                {
+                    source[offset] = (byte)value;
+                    source[offset + 1] = (byte)(value >> 8);
+                    source[offset + 2] = (byte)(value >> 16);
+                }
+                else
+                {
+                    source[offset] = (byte)(value >> 16);
+                    source[offset + 1] = (byte)(value >> 8);
+                    source[offset + 2] = (byte)value;
+                }
             }
 
             public void WriteInt16(short value, int offset = 0, bool littleEndian = true)
@@ -62,6 +84,30 @@ namespace Basalt.Binary
                     value = BinaryPrimitives.ReverseEndianness(value);
                 MemoryMarshal.Write(source[offset..], value);
             }
+
+            public void WriteF16(Half value, int offset = 0, bool littleEndian = true)
+            {
+                short bits = BitConverter.HalfToInt16Bits(value);
+                if (IsLittleEndian != littleEndian)
+                    bits = BinaryPrimitives.ReverseEndianness(bits);
+                MemoryMarshal.Write(source[offset..], bits);
+            }
+
+            public void WriteF32(float value, int offset = 0, bool littleEndian = true)
+            {
+                int bits = BitConverter.SingleToInt32Bits(value);
+                if (IsLittleEndian != littleEndian)
+                    bits = BinaryPrimitives.ReverseEndianness(bits);
+                MemoryMarshal.Write(source[offset..], bits);
+            }
+
+            public void WriteF64(double value, int offset = 0, bool littleEndian = true)
+            {
+                long bits = BitConverter.DoubleToInt64Bits(value);
+                if (IsLittleEndian != littleEndian)
+                    bits = BinaryPrimitives.ReverseEndianness(bits);
+                MemoryMarshal.Write(source[offset..], bits);
+            }
         }
         extension(ReadOnlySpan<byte> source)
         {
@@ -73,6 +119,18 @@ namespace Basalt.Binary
             public byte ReadUInt8(int offset = 0)
             {
                 return source[offset];
+            }
+
+            public bool ReadBool(int offset = 0)
+            {
+                return source[offset] != 0;
+            }
+
+            public uint ReadUInt24(int offset = 0, bool littleEndian = true)
+            {
+                if (littleEndian)
+                    return (uint)(source[offset] | (source[offset + 1] << 8) | (source[offset + 2] << 16));
+                return (uint)((source[offset] << 16) | (source[offset + 1] << 8) | source[offset + 2]);
             }
 
             public short ReadInt16(int offset = 0, bool littleEndian = true)
@@ -121,6 +179,30 @@ namespace Basalt.Binary
                 if (IsLittleEndian != littleEndian)
                     value = BinaryPrimitives.ReverseEndianness(value);
                 return value;
+            }
+
+            public Half ReadF16(int offset = 0, bool littleEndian = true)
+            {
+                short bits = MemoryMarshal.Read<short>(source[offset..]);
+                if (IsLittleEndian != littleEndian)
+                    bits = BinaryPrimitives.ReverseEndianness(bits);
+                return BitConverter.Int16BitsToHalf(bits);
+            }
+
+            public float ReadF32(int offset = 0, bool littleEndian = true)
+            {
+                int bits = MemoryMarshal.Read<int>(source[offset..]);
+                if (IsLittleEndian != littleEndian)
+                    bits = BinaryPrimitives.ReverseEndianness(bits);
+                return BitConverter.Int32BitsToSingle(bits);
+            }
+
+            public double ReadF64(int offset = 0, bool littleEndian = true)
+            {
+                long bits = MemoryMarshal.Read<long>(source[offset..]);
+                if (IsLittleEndian != littleEndian)
+                    bits = BinaryPrimitives.ReverseEndianness(bits);
+                return BitConverter.Int64BitsToDouble(bits);
             }
         }
     }
