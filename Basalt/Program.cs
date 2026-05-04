b7915dede07f344a807f59901a07a9bfc6b31b8e
con <conmaster@noreply.com>
2026-05-04 19:17:40 +0200

    stylistic

--- Basalt/Program.cs
+++ Basalt/Program.cs
@@ -6,7 +6,6 @@ namespace Basalt.Core
     {
         static void Main(string[] args)
         {
- 
             // no suggestions for me nothing wont even show errors            NetworkServer server = new();
             // ok, f it, do you want to continue or not?
 
