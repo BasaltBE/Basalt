5220ca6dd6ecc054f610d1cd4bd6eae9296aa23f
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-05 16:42:02 +0300

    RequestNetworkSettings packte

---
+++ Protocol/Packets/RequestNetworkSettings.cs
@@ -0,0 +1,31 @@
+using Basalt.Protocol.Enums;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Packets;
+
+/// <summary>
+/// Sent by the client to request the server's network settings
+/// Includes the client's protocol version that is used to check if the 
+/// server is compatible
+/// </summary>
+public sealed record RequestNetworkSettingsPacket : DataPacket
+{
+    public RequestNetworkSettingsPacket(int protocolVersion = 0)
+    {
+        ProtocolVersion = protocolVersion;
+    }
+
+    public int ProtocolVersion { get; set; }
+    public override PacketId PacketId => PacketId.RequestNetworkSettings;
+
+    public override void Deserialize(ref BinaryReader reader)
+    {
+        ProtocolVersion = reader.ReadInt32();
+    }
+
+    public override void Serialize(ref BinaryWriter writer)
+    {
+        writer.WriteInt32(ProtocolVersion);
+    }
+}
