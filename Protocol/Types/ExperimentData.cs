e41fcffc8d920947daca9a55b49dc4802d36c043
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-06 00:25:37 +0300

    ExperimentData type

---
+++ Protocol/Types/ExperimentData.cs
@@ -0,0 +1,25 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class ExperimentData
+{
+    public string Name { get; set; } = string.Empty;
+    public bool Enabled { get; set; }
+
+    public void Read(ref BinaryReader reader)
+    {
+        Name = reader.ReadVarString();
+        Enabled = reader.ReadBool();
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteVarString(Name);
+        writer.WriteBool(Enabled);
+    }
+
+    public void Deserialize(ref BinaryReader reader) => Read(ref reader);
+    public void Serialize(ref BinaryWriter writer) => Write(ref writer);
+}
