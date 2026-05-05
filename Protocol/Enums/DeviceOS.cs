26071c9966dacbb5e6024491284cfda3fadfeb51
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-05 23:39:19 +0300

    Implement DeviceOS enum

---
+++ Protocol/Enums/DeviceOS.cs
@@ -0,0 +1,21 @@
+namespace Basalt.Protocol.Enums;
+
+public enum DeviceOS : int
+{
+    Undefined,
+    Android,
+    Ios,
+    MacOS,
+    FireOS,
+    GearVR,
+    Hololens,
+    Win10,
+    Win32,
+    Dedicated,
+    TVOS,
+    Orbis,
+    Switch,
+    Xbox,
+    WindowsPhone,
+    Linux
+}
