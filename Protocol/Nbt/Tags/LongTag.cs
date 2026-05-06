45276c6e4d0c9bf01e80e4096739d504fdfe8041
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-06 13:11:57 +0300

    Implement LongTag

---
+++ Protocol/Nbt/Tags/LongTag.cs
@@ -0,0 +1,31 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Nbt;
+
+public sealed class LongTag : BaseTag
+{
+    public override TagType Type => TagType.Long;
+    public long Value { get; set; }
+    public override object ToJsonValue() => Value;
+
+    public override void Read(ref BinaryReader reader, ReadWriteOptions options, bool canHaveName = true)
+    {
+        if (canHaveName && options.Name)
+        {
+            Name = ReadName(ref reader, options.VarInt);
+        }
+
+        Value = reader.ReadInt64(true);
+    }
+
+    public override void Write(ref BinaryWriter writer, ReadWriteOptions options, bool canHaveName = true)
+    {
+        if (canHaveName && options.Name)
+        {
+            WriteName(ref writer, Name, options.VarInt);
+        }
+
+        writer.WriteInt64(Value, true);
+    }
+}
