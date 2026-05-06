0a739a4d22911256fc3d7f6f42ab0e2574932d84
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 00:32:34 +0300

    Add InMemoryProvider for chunk management in memory

---
+++ Basalt/World/Dimension/Provider/InMemoryProvider.cs
@@ -0,0 +1,31 @@
+using Basalt.Protocol.Enums;
+using ChunkColumn = Basalt.World.Dimension.Chunk.Chunk;
+
+namespace Basalt.World.Dimension.Provider;
+
+public sealed class InMemoryProvider : WorldProvider
+{
+    private readonly Dictionary<long, ChunkColumn> _chunks = [];
+    public override string Identifier => "memory";
+    public override bool HasChunk(int x, int z) => _chunks.ContainsKey(HashChunk(x, z));
+
+    public override ChunkColumn? LoadChunk(DimensionType dimensionType, int x, int z)
+    {
+        _chunks.TryGetValue(HashChunk(x, z), out ChunkColumn? chunk);
+        return chunk;
+    }
+
+    public override void SaveChunk(ChunkColumn chunk)
+    {
+        _chunks[HashChunk(chunk.X, chunk.Z)] = chunk;
+    }
+
+    public override void DeleteChunk(int x, int z)
+    {
+        _chunks.Remove(HashChunk(x, z));
+    }
+
+    public override void Dispose()
+    {
+    }
+}
