a35038be2f1ed77d8ef2c31f9420f692ad2f8d6a
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-06 16:11:37 +0300

    Add new packet IDs

--- Protocol/Enums/PacketId.cs
+++ Protocol/Enums/PacketId.cs
@@ -8,6 +8,8 @@ namespace Basalt.Protocol.Enums
         ResourcePacksInfo = 0x06,
         ResourcePackStack = 0x07,
         ResourcePackClientResponse = 0x08,
+        StartGame = 0x0b,
+        PlayerAuthInput = 0x90,
         NetworkSettings = 0x8f,
         RequestNetworkSettings = 0xc1
     }
