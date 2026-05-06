867f541014b1968d4097e6a05182acc6cbfe65df
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 00:30:02 +0300

    Add missing packet IDs for LevelChunk and related updates

--- Protocol/Enums/PacketId.cs
+++ Protocol/Enums/PacketId.cs
@@ -9,6 +9,12 @@ namespace Basalt.Protocol.Enums
         ResourcePackStack = 0x07,
         ResourcePackClientResponse = 0x08,
         StartGame = 0x0b,
+        LevelChunk = 0x3a,
+        RequestChunkRadius = 0x45,
+        ChunkRadiusUpdated = 0x46,
+        SetLocalPlayerAsInitialized = 0x71,
+        NetworkStackLatency = 0x73,
+        NetworkChunkPublisherUpdate = 0x79,
         PlayerAuthInput = 0x90,
         NetworkSettings = 0x8f,
         RequestNetworkSettings = 0xc1
