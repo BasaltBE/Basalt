713b887904105a4bf297aae72499d4163934fdc0
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-06 13:10:56 +0300

    Implement EndTag

---
+++ Protocol/Nbt/Tags/EndTag.cs
@@ -0,0 +1,26 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Nbt;
+
+public sealed class EndTag : BaseTag
+{
+    public override TagType Type => TagType.End;
+    public override object? ToJsonValue() => null;
+
+    public override void Read(ref BinaryReader reader, ReadWriteOptions options, bool canHaveName = true)
+    {
+        if (canHaveName && options.Name)
+        {
+            Name = ReadName(ref reader, options.VarInt);
+        }
+    }
+
+    public override void Write(ref BinaryWriter writer, ReadWriteOptions options, bool canHaveName = true)
+    {
+        if (canHaveName && options.Name)
+        {
+            WriteName(ref writer, Name, options.VarInt);
+        }
+    }
+}
