653495c1b8a54c751ad6f5e96e442f5d970b142f
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-06 16:05:56 +0300

    Implement Optional type but i might redo this later

---
+++ Protocol/Types/OptionalValue.cs
@@ -0,0 +1,41 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class OptionalValue<T>
+{
+    public delegate T ReaderDelegate(ref BinaryReader reader);
+    public delegate void WriterDelegate(ref BinaryWriter writer, T value);
+
+    public bool HasValue { get; set; }
+    public T? Value { get; set; }
+
+    public void Deserialize(ref BinaryReader reader, ReaderDelegate read)
+    {
+        HasValue = reader.ReadBool();
+        if (!HasValue)
+        {
+            Value = default;
+            return;
+        }
+
+        Value = read(ref reader);
+    }
+
+    public void Serialize(ref BinaryWriter writer, WriterDelegate write)
+    {
+        writer.WriteBool(HasValue);
+        if (!HasValue)
+        {
+            return;
+        }
+
+        if (Value is null)
+        {
+            throw new InvalidOperationException("Optional value is marked as present but Value is null.");
+        }
+
+        write(ref writer, Value);
+    }
+}
