163582f3deaab5033db0c36ef3a9795cace8b166
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 01:01:29 +0300

    Add methods for world creation and dimension management in Server class

--- Basalt/Server.cs
+++ Basalt/Server.cs
@@ -1,5 +1,9 @@
 using Basalt.Network;
+using Basalt.Protocol.Enums;
 using Basalt.RakNet;
+using Basalt.World.Dimension.Provider;
+using Basalt.World.Dimension.Generation;
+using WorldInstance = Basalt.World.World;
 
 namespace Basalt.Core;
 
@@ -12,6 +16,7 @@ public sealed class Server
 
     public ServerOptions Options { get; }
     public NetworkHandler Network => _network;
+    public WorldInstance World { get; }
 
     public Server(ServerOptions options = default)
     {
@@ -20,10 +25,54 @@ public sealed class Server
         _network = new NetworkHandler(this);
         _raknet.OnMessage += _network.HandlePacket;
         _raknet.OnDisconnected += _network.HandleDisconnected;
+
+        World = CreateDefaultWorld();
     }
 
     public void Start()
     {
         _raknet.Start().AsTask().Wait();
+    }
+
+    public WorldInstance CreateWorld(string name, string providerIdentifier, params object[] providerArgs)
+    {
+        if (string.IsNullOrWhiteSpace(providerIdentifier))
+        {
+            throw new ArgumentException("Provider identifier cannot be empty.", nameof(providerIdentifier));
+        }
+
+        Type providerType = providerIdentifier.ToLowerInvariant() switch
+        {
+            "leveldb" => typeof(LevelDbProvider),
+            "memory" => typeof(InMemoryProvider),
+            _ => throw new KeyNotFoundException($"Unknown provider identifier '{providerIdentifier}'.")
+        };
+
+        return CreateWorld(name, providerType, providerArgs);
+    }
+
+    public WorldInstance CreateWorld(string name, Type providerType, params object[] providerArgs)
+    {
+        if (!typeof(WorldProvider).IsAssignableFrom(providerType))
+        {
+            throw new ArgumentException($"Provider type must inherit {nameof(WorldProvider)}.", nameof(providerType));
+        }
+
+        object? providerInstance = Activator.CreateInstance(providerType, providerArgs);
+        if (providerInstance is not WorldProvider provider)
+        {
+            throw new InvalidOperationException($"Could not construct provider '{providerType.FullName}'.");
+        }
+
+        return new WorldInstance(name, provider);
+    }
+
+    private WorldInstance CreateDefaultWorld()
+    {
+        WorldInstance world = CreateWorld("world", "leveldb", "worlds/world");
+        world.RegisterGenerator<VoidGenerator>("void");
+        world.RegisterGenerator<SuperFlatGenerator>("superflat");
+        world.CreateDimension("overworld", DimensionType.Overworld, "superflat");
+        return world;
     }
 }
