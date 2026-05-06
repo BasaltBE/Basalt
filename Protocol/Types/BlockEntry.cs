b0beca201454e9d4e1b2bdb574b440235bce0697
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-06 16:07:09 +0300

    Implement BlockEntry

---
+++ Protocol/Types/BlockEntry.cs
@@ -0,0 +1,26 @@
+using Basalt.Protocol.IO;
+using Basalt.Protocol.Nbt;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class BlockEntry
+{
+    private static readonly ReadWriteOptions NetworkOptions = new(Name: true, Type: true, VarInt: true);
+
+    public string Name { get; set; } = string.Empty;
+    public CompoundTag Properties { get; set; } = new();
+
+    public void Read(ref BinaryReader reader)
+    {
+        Name = reader.ReadVarString();
+        Properties = CompoundTag.Read(ref reader, NetworkOptions, canHaveName: true);
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteVarString(Name);
+        NBT.WriteTag(ref writer, Properties, NetworkOptions, canHaveName: true);
+    }
+}
