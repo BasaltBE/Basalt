9b8a1e193ced062b0987ad7d7a97922fa5e3b5e3
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-06 16:05:28 +0300

    Implement PlayerMovementSettings

---
+++ Protocol/Types/PlayerMovementSettings.cs
@@ -0,0 +1,22 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class PlayerMovementSettings
+{
+    public int RewindHistorySize { get; set; }
+    public bool ServerAuthoritativeBlockBreaking { get; set; }
+
+    public void Read(ref BinaryReader reader)
+    {
+        RewindHistorySize = reader.ReadZigZag();
+        ServerAuthoritativeBlockBreaking = reader.ReadBool();
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteZigZag(RewindHistorySize);
+        writer.WriteBool(ServerAuthoritativeBlockBreaking);
+    }
+}
