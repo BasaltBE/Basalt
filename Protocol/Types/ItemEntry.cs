b5c3a41cd39887d72fde7ee1382cb3ca9d409423
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 14:47:16 +0300

    Add ItemEntry class for handling item data serialization and deserialization

---
+++ Protocol/Types/ItemEntry.cs
@@ -0,0 +1,35 @@
+using Basalt.Protocol.IO;
+using Basalt.Protocol.Nbt;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class ItemEntry : DataType
+{
+    private static readonly ReadWriteOptions NetworkNbtOptions = new(Name: true, Type: true, VarInt: true);
+
+    public string Name { get; set; } = string.Empty;
+    public short RuntimeId { get; set; }
+    public bool ComponentBased { get; set; }
+    public int Version { get; set; }
+    public CompoundTag Data { get; set; } = new();
+
+    public void Read(ref BinaryReader reader)
+    {
+        Name = reader.ReadVarString();
+        RuntimeId = reader.ReadInt16(true);
+        ComponentBased = reader.ReadBool();
+        Version = reader.ReadVarInt();
+        Data = CompoundTag.Read(ref reader, NetworkNbtOptions, canHaveName: true);
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteVarString(Name);
+        writer.WriteInt16(RuntimeId, true);
+        writer.WriteBool(ComponentBased);
+        writer.WriteVarInt(Version);
+        NBT.WriteTag(ref writer, Data, NetworkNbtOptions, canHaveName: true);
+    }
+}
