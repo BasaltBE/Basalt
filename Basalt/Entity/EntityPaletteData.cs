1dddca42f94915daaaad4dd6ac15964215f2ce40
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 17:00:30 +0300

    Add EntityTypeData class and EntityPaletteJsonContext for entity palette serialization

---
+++ Basalt/Entity/EntityPaletteData.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace Basalt.Entity;
+
+public sealed class EntityTypeData
+{
+    [JsonPropertyName("identifier")]
+    public string Identifier { get; set; } = string.Empty;
+
+    [JsonPropertyName("components")]
+    public List<string> Components { get; set; } = [];
+}
+
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = false)]
+[JsonSerializable(typeof(List<EntityTypeData>))]
+internal partial class EntityPaletteJsonContext : JsonSerializerContext
+{
+}
