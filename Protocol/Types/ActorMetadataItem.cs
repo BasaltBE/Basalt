2811c8ce789052fdcf22e4de84881946d77f1398
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 18:25:40 +0300

    Add ActorMetadataItem class for managing actor metadata items

---
+++ Protocol/Types/ActorMetadataItem.cs
@@ -0,0 +1,74 @@
+using Basalt.Protocol.Enums;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class ActorMetadataItem : DataType
+{
+    public ActorDataId Id { get; set; }
+    public ActorDataType Type { get; set; }
+    public object Value { get; set; } = 0;
+
+    public void Read(ref BinaryReader reader)
+    {
+        Id = (ActorDataId)reader.ReadVarUInt();
+        Type = (ActorDataType)reader.ReadVarUInt();
+        Value = Type switch
+        {
+            ActorDataType.Byte => reader.ReadInt8(),
+            ActorDataType.Short => reader.ReadInt16(true),
+            ActorDataType.Int => reader.ReadVarInt(),
+            ActorDataType.Float => reader.ReadF32(true),
+            ActorDataType.String => reader.ReadVarString(),
+            ActorDataType.Long => reader.ReadVarLong(),
+            ActorDataType.Vec3 =>
+                new Vec3f
+                {
+                    X = reader.ReadF32(true),
+                    Y = reader.ReadF32(true),
+                    Z = reader.ReadF32(true)
+                },
+            _ => throw new NotSupportedException($"Unsupported ActorDataType: {Type}")
+        };
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteVarUInt((uint)Id);
+        writer.WriteVarUInt((uint)Type);
+        switch (Type)
+        {
+            case ActorDataType.Byte:
+                writer.WriteInt8(Convert.ToSByte(Value));
+                break;
+            case ActorDataType.Short:
+                writer.WriteInt16(Convert.ToInt16(Value), true);
+                break;
+            case ActorDataType.Int:
+                writer.WriteVarInt(Convert.ToInt32(Value));
+                break;
+            case ActorDataType.Float:
+                writer.WriteF32(Convert.ToSingle(Value), true);
+                break;
+            case ActorDataType.String:
+                writer.WriteVarString(Convert.ToString(Value) ?? string.Empty);
+                break;
+            case ActorDataType.Long:
+                writer.WriteVarLong(Convert.ToInt64(Value));
+                break;
+            case ActorDataType.Vec3:
+                if (Value is not Vec3f vec3)
+                {
+                    throw new InvalidOperationException("Actor metadata Vec3 value must be Vec3f.");
+                }
+
+                writer.WriteF32(vec3.X, true);
+                writer.WriteF32(vec3.Y, true);
+                writer.WriteF32(vec3.Z, true);
+                break;
+            default:
+                throw new NotSupportedException($"Unsupported ActorDataType: {Type}");
+        }
+    }
+}
