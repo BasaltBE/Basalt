61fa8aaeaaea093510a168a4fd90f393825bc8fa
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 15:01:52 +0300

    Add MismatchInventoryTransactionData class for handling mismatch inventory transactions

---
+++ Protocol/Types/MismatchInventoryTransactionData.cs
@@ -0,0 +1,18 @@
+using Basalt.Protocol.Enums;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class MismatchInventoryTransactionData : IInventoryTransactionData
+{
+    public InventoryTransactionType Type => InventoryTransactionType.Mismatch;
+
+    public void Read(ref BinaryReader reader)
+    {
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+    }
+}
