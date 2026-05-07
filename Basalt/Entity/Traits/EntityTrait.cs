c12e51c5e27bd2412414b7858fc03bf995ab297a
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 17:00:15 +0300

    Add EntityTrait abstract class for entity behavior management

---
+++ Basalt/Entity/Traits/EntityTrait.cs
@@ -0,0 +1,75 @@
+using Basalt.Commands;
+using Basalt.Containers;
+using Basalt.Core;
+using Basalt.Entity.Traits.Enums;
+using Basalt.Entity.Traits.Types;
+using Basalt.Protocol.Enums;
+using Basalt.Traits;
+using Basalt.World.Dimension;
+
+namespace Basalt.Entity.Traits;
+
+public abstract class EntityTrait : Trait
+{
+    public static ReadOnlySpan<EntityIdentifier> Types => [];
+    public static ReadOnlySpan<string> Components => [];
+
+    protected Entity Entity { get; }
+
+    protected Dimension Dimension => Entity.Dimension
+        ?? throw new InvalidOperationException("Entity is not in a dimension.");
+
+    protected EntityTrait(Entity entity)
+    {
+        Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+    }
+
+    public virtual void OnSpawn(EntitySpawnOptions details)
+    {
+    }
+
+    public virtual void OnDespawn(EntityDespawnOptions details)
+    {
+    }
+
+    public virtual void OnDeath(EntityDeathOptions details)
+    {
+    }
+
+    public virtual void OnTeleport(EntityTeleportOptions details)
+    {
+    }
+
+    public virtual void OnInteract(Player player, EntityInteractMethod method)
+    {
+    }
+
+    public virtual bool OnCommand(CommandExecutionState state)
+    {
+        return true;
+    }
+
+    public virtual void OnContainerUpdate(Container container)
+    {
+    }
+
+    public virtual void OnFallOnBlock(EntityFallOnBlockTraitEvent @event)
+    {
+    }
+
+    public virtual void OnRendered(EntityRenderedOptions details)
+    {
+    }
+
+    public abstract EntityTrait Clone(Entity entity);
+
+    public override Trait Clone(params object?[] args)
+    {
+        if (args.Length != 1 || args[0] is not Entity entity)
+        {
+            throw new ArgumentException("EntityTrait.Clone requires exactly one Entity argument.", nameof(args));
+        }
+
+        return Clone(entity);
+    }
+}
