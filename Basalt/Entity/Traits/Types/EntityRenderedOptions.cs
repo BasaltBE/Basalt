0663c7407ee4d9746562fc10eb845f1084d3037d
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 18:41:56 +0300

    Fix namespace reference for Player in EntityRenderedOptions

--- Basalt/Entity/Traits/Types/EntityRenderedOptions.cs
+++ Basalt/Entity/Traits/Types/EntityRenderedOptions.cs
@@ -1,5 +1,3 @@
-using Basalt.Core;
-
 namespace Basalt.Entity.Traits.Types;
 
-public readonly record struct EntityRenderedOptions(Player Viewer);
+public readonly record struct EntityRenderedOptions(global::Basalt.Core.Player Viewer);
