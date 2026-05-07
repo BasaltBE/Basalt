8a7607f5299418557fdbd562ea1a01a310509327
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 15:06:04 +0300

    Add ItemType class for item management with registry and network support

---
+++ Basalt/Item/ItemType.cs
@@ -0,0 +1,96 @@
+using Basalt.Block;
+using Basalt.Protocol.Nbt;
+using Basalt.Protocol.Types;
+
+namespace Basalt.Item;
+
+public sealed class ItemType
+{
+    private static readonly Dictionary<string, ItemType> Registry = new(StringComparer.Ordinal);
+    private static readonly Dictionary<int, ItemType> NetworkRegistry = [];
+
+    public string Identifier { get; }
+    public int NetworkId { get; }
+    public int MaxStackSize { get; }
+    public bool IsComponentBased { get; }
+    public int Version { get; }
+    public IReadOnlyList<string> Tags { get; }
+    public CompoundTag Properties { get; }
+    public BlockType? BlockType { get; }
+
+    public static IReadOnlyDictionary<string, ItemType> Types => Registry;
+    public static ItemType Air => GetOrAir("minecraft:air");
+
+    public ItemType(
+        string identifier,
+        int networkId,
+        int maxStackSize,
+        IEnumerable<string>? tags,
+        bool isComponentBased,
+        int version,
+        CompoundTag? properties = null)
+    {
+        Identifier = identifier;
+        NetworkId = networkId;
+        MaxStackSize = maxStackSize;
+        IsComponentBased = isComponentBased;
+        Version = version;
+        Tags = tags is null ? [] : [.. tags];
+        Properties = properties ?? new CompoundTag();
+        BlockType = Block.BlockType.Get(identifier);
+
+        Registry[identifier] = this;
+        NetworkRegistry[networkId] = this;
+    }
+
+    public static ItemType? Get(string identifier)
+    {
+        return Registry.TryGetValue(identifier, out ItemType? type) ? type : null;
+    }
+
+    public static ItemType GetOrAir(string identifier)
+    {
+        return Get(identifier) ?? Get("minecraft:air") ?? new ItemType("minecraft:air", 0, 64, [], true, 1);
+    }
+
+    public static ItemType? GetByNetwork(int networkId)
+    {
+        return NetworkRegistry.TryGetValue(networkId, out ItemType? type) ? type : null;
+    }
+
+    public static List<ItemType> GetAll()
+    {
+        return [.. Registry.Values];
+    }
+
+    public static void EnsureRegistryCapacity(int capacity)
+    {
+        Registry.EnsureCapacity(capacity);
+        NetworkRegistry.EnsureCapacity(capacity);
+    }
+
+    public static NetworkItemStackDescriptor ToNetworkStack(ItemType type, ushort stackSize = 1, uint metadata = 0)
+    {
+        int networkBlockId = 0;
+        if (type.BlockType is not null && type.BlockType.Permutations.Count > 0)
+        {
+            networkBlockId = type.BlockType.Permutations[0].NetworkId;
+        }
+
+        return new NetworkItemStackDescriptor
+        {
+            NetworkId = type.NetworkId,
+            StackSize = stackSize,
+            Metadata = metadata,
+            ItemStackId = null,
+            NetworkBlockId = networkBlockId,
+            ExtraData = new ItemInstanceUserData
+            {
+                Nbt = null,
+                CanPlaceOn = [],
+                CanDestroy = [],
+                Ticking = null
+            }
+        };
+    }
+}
