07444703f7d2cafee57ccf1ada9e43c07941164a
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 18:40:37 +0300

    Add AttributeName enum and update AttributeNameHelper for protocol string mapping

--- Protocol/Enums/AttributeNameHelper.cs
+++ Protocol/Enums/AttributeNameHelper.cs
@@ -4,14 +4,23 @@ public static class AttributeNameHelper
 {
     public static AttributeName FromProtocolString(string value) => value switch
     {
-        "minecraft:health" => AttributeName.Health,
-        "minecraft:movement" => AttributeName.Movement,
         "minecraft:absorption" => AttributeName.Absorption,
-        "minecraft:player.hunger" => AttributeName.Hunger,
-        "minecraft:player.saturation" => AttributeName.Saturation,
-        "minecraft:player.exhaustion" => AttributeName.Exhaustion,
         "minecraft:attack_damage" => AttributeName.AttackDamage,
+        "minecraft:fall_damage" => AttributeName.FallDamage,
+        "minecraft:follow_range" => AttributeName.FollowRange,
+        "minecraft:health" => AttributeName.Health,
+        "minecraft:horse.jump_strength" => AttributeName.HorseJumpStrength,
         "minecraft:knockback_resistance" => AttributeName.KnockbackResistance,
+        "minecraft:lava_movement" => AttributeName.LavaMovement,
+        "minecraft:luck" => AttributeName.Luck,
+        "minecraft:movement" => AttributeName.Movement,
+        "minecraft:player.exhaustion" => AttributeName.PlayerExhaustion,
+        "minecraft:player.experience" => AttributeName.PlayerExperience,
+        "minecraft:player.hunger" => AttributeName.PlayerHunger,
+        "minecraft:player.level" => AttributeName.PlayerLevel,
+        "minecraft:player.saturation" => AttributeName.PlayerSaturation,
+        "minecraft:underwater_movement" => AttributeName.UnderwaterMovement,
+        "minecraft:zombie.spawn_reinforcements" => AttributeName.ZombieSpawnReinforcements,
         _ => AttributeName.Unknown
     };
 }
