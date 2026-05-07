c1bab1fbe57f9394d7d2aabdabe72635537cd260
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 19:19:56 +0300

    Add EntityAttributeTrait to manage entity attributes and their properties

---
+++ Basalt/Entity/Traits/EntityAttributeTrait.cs
@@ -0,0 +1,104 @@
+using Basalt.Entity.Traits.Types;
+using Basalt.Protocol.Enums;
+using ProtoAttribute = Basalt.Protocol.Types.Attribute;
+
+namespace Basalt.Entity.Traits;
+
+public abstract class EntityAttributeTrait : EntityTrait
+{
+    private readonly AttributeProperties? _initialProperties;
+
+    public abstract AttributeName Attribute { get; }
+
+    public bool Sync { get; set; } = true;
+
+    public float MinimumValue
+    {
+        get => GetAttribute().Min;
+        set
+        {
+            ProtoAttribute attribute = GetAttribute();
+            attribute.Min = Truncate4(value);
+            Entity.Attributes.SetAttribute(attribute);
+        }
+    }
+
+    public float MaximumValue
+    {
+        get => GetAttribute().Max;
+        set
+        {
+            ProtoAttribute attribute = GetAttribute();
+            attribute.Max = Truncate4(value);
+            Entity.Attributes.SetAttribute(attribute);
+        }
+    }
+
+    public float DefaultValue
+    {
+        get => GetAttribute().Default;
+        set
+        {
+            ProtoAttribute attribute = GetAttribute();
+            attribute.Default = Truncate4(value);
+            Entity.Attributes.SetAttribute(attribute);
+        }
+    }
+
+    public float CurrentValue
+    {
+        get => GetAttribute().Current;
+        set
+        {
+            ProtoAttribute attribute = GetAttribute();
+            attribute.Current = Truncate4(value);
+            Entity.Attributes.SetAttribute(attribute);
+        }
+    }
+
+    protected EntityAttributeTrait(Entity entity, AttributeProperties? properties = null) : base(entity)
+    {
+        _initialProperties = properties;
+    }
+
+    public ProtoAttribute GetAttribute()
+    {
+        return Entity.Attributes.GetAttribute(Attribute)
+            ?? throw new InvalidOperationException($"Attribute {Attribute} is not registered on entity.");
+    }
+
+    public void Reset()
+    {
+        CurrentValue = DefaultValue;
+    }
+
+    public override void OnAdd()
+    {
+        EnsureAttribute(_initialProperties ?? new AttributeProperties());
+    }
+
+    protected void EnsureAttribute(AttributeProperties properties)
+    {
+        if (Entity.Attributes.HasAttribute(Attribute))
+        {
+            return;
+        }
+
+        float min = Truncate4(properties.MinimumValue ?? 0f);
+        float max = Truncate4(properties.MaximumValue ?? 0f);
+        float @default = Truncate4(properties.DefaultValue ?? 0f);
+        float current = Truncate4(properties.CurrentValue ?? @default);
+
+        Entity.Attributes.SetAttribute(new ProtoAttribute(min, max, current, @default, Attribute));
+    }
+
+    public override void OnRemove()
+    {
+        _ = Entity.Attributes.RemoveAttribute(Attribute);
+    }
+
+    private static float Truncate4(float value)
+    {
+        return MathF.Truncate(value * 10000f) / 10000f;
+    }
+}
