792ecdbbc957b25144df272f37a344cce35099fb
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 14:25:08 +0300

    Add ShieldNetworkId constant to ProtocolInfo for network identification

--- Protocol/ProtocolInfo.cs
+++ Protocol/ProtocolInfo.cs
@@ -4,4 +4,5 @@ public static class ProtocolInfo
 {
     public const int ProtocolVersion = 975;
     public const string MinecraftVersion = "1.26.20";
+    public const int ShieldNetworkId = 355;
 }
