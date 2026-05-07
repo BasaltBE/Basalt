ad0a30f24ef3748caaafbfb92adb5f1960c9b3d4
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 15:00:44 +0300

    Add ItemStackResponse class for handling item stack responses

---
+++ Protocol/Types/ItemStackResponse.cs
@@ -0,0 +1,50 @@
+using Basalt.Protocol.Enums;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class ItemStackResponse : DataType
+{
+    public ItemStackResponseStatus Status { get; set; }
+    public int RequestId { get; set; }
+    public List<StackResponseContainerInfo> ContainerInfo { get; set; } = [];
+
+    public void Read(ref BinaryReader reader)
+    {
+        Status = (ItemStackResponseStatus)reader.ReadUInt8();
+        RequestId = reader.ReadZigZag();
+
+        if (Status != ItemStackResponseStatus.Ok)
+        {
+            ContainerInfo = [];
+            return;
+        }
+
+        int count = checked((int)reader.ReadVarUInt());
+        ContainerInfo = new(count);
+        for (int i = 0; i < count; i++)
+        {
+            StackResponseContainerInfo info = new();
+            info.Read(ref reader);
+            ContainerInfo.Add(info);
+        }
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteUInt8((byte)Status);
+        writer.WriteZigZag(RequestId);
+
+        if (Status != ItemStackResponseStatus.Ok)
+        {
+            return;
+        }
+
+        writer.WriteVarUInt((uint)ContainerInfo.Count);
+        for (int i = 0; i < ContainerInfo.Count; i++)
+        {
+            ContainerInfo[i].Write(ref writer);
+        }
+    }
+}
