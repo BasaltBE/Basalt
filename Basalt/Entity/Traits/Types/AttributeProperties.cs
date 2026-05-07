b769c76f07f04d38325c0a422412bb53a5922351
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 18:45:45 +0300

    Add AttributeProperties record struct to define attribute value ranges

---
+++ Basalt/Entity/Traits/Types/AttributeProperties.cs
@@ -0,0 +1,8 @@
+namespace Basalt.Entity.Traits.Types;
+
+public readonly record struct AttributeProperties(
+    float? MinimumValue = null,
+    float? MaximumValue = null,
+    float? DefaultValue = null,
+    float? CurrentValue = null
+);
