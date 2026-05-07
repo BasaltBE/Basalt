a42eb4d50b750e7a9a4f4c2c5545cc2f0cd7bcab
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 14:24:56 +0300

    Reorder PacketId enum values for clarity and consistency

--- Protocol/Enums/PacketId.cs
+++ Protocol/Enums/PacketId.cs
@@ -9,14 +9,16 @@ namespace Basalt.Protocol.Enums
         ResourcePackStack = 0x07,
         ResourcePackClientResponse = 0x08,
         StartGame = 0x0b,
+        MovePlayer = 0x13,
         LevelChunk = 0x3a,
         RequestChunkRadius = 0x45,
         ChunkRadiusUpdated = 0x46,
         SetLocalPlayerAsInitialized = 0x71,
         NetworkStackLatency = 0x73,
         NetworkChunkPublisherUpdate = 0x79,
-        PlayerAuthInput = 0x90,
         NetworkSettings = 0x8f,
+        PlayerAuthInput = 0x90,
+        CorrectPlayerMovePrediction = 0xa1,
         RequestNetworkSettings = 0xc1
     }
 }
