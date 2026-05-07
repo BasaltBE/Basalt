ec288c66ed34dc655f08df14fd6648885bd3111f
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-07 14:16:54 +0300

    Update optional value

--- Protocol/Types/Optional.cs
+++ Protocol/Types/Optional.cs
@@ -34,5 +34,45 @@ public sealed class Optional<T> : OptionalValue<T> where T : DataType, new()
 
         Value.Write(ref writer);
     }
+
+    public void Read<TParameter>(ref BinaryReader reader, TParameter parameter)
+    {
+        HasValue = reader.ReadBool();
+        if (!HasValue)
+        {
+            Value = default;
+            return;
+        }
+
+        T value = new();
+        if (value is not DataType<TParameter> parameterized)
+        {
+            throw new InvalidOperationException($"{typeof(T).Name} does not implement DataType<{typeof(TParameter).Name}>.");
+        }
+
+        parameterized.Read(ref reader, parameter);
+        Value = value;
+    }
+
+    public void Write<TParameter>(ref BinaryWriter writer, TParameter parameter)
+    {
+        writer.WriteBool(HasValue);
+        if (!HasValue)
+        {
+            return;
+        }
+
+        if (Value is null)
+        {
+            throw new InvalidOperationException("Optional value is marked as present but Value is null.");
+        }
+
+        if (Value is not DataType<TParameter> parameterized)
+        {
+            throw new InvalidOperationException($"{typeof(T).Name} does not implement DataType<{typeof(TParameter).Name}>.");
+        }
+
+        parameterized.Write(ref writer, parameter);
+    }
 }
 
--- Protocol/Types/OptionalValue.cs
+++ Protocol/Types/OptionalValue.cs
@@ -6,7 +6,9 @@ namespace Basalt.Protocol.Types;
 public class OptionalValue<T>
 {
     public delegate T ReaderDelegate(ref BinaryReader reader);
+    public delegate T ReaderDelegate<TParameter>(ref BinaryReader reader, TParameter parameter);
     public delegate void WriterDelegate(ref BinaryWriter writer, T value);
+    public delegate void WriterDelegate<TParameter>(ref BinaryWriter writer, T value, TParameter parameter);
 
     public bool HasValue { get; set; }
     public T? Value { get; set; }
@@ -37,6 +39,34 @@ public class OptionalValue<T>
         }
 
         write(ref writer, Value);
+    }
+
+    public void Read<TParameter>(ref BinaryReader reader, TParameter parameter, ReaderDelegate<TParameter> read)
+    {
+        HasValue = reader.ReadBool();
+        if (!HasValue)
+        {
+            Value = default;
+            return;
+        }
+
+        Value = read(ref reader, parameter);
+    }
+
+    public void Write<TParameter>(ref BinaryWriter writer, TParameter parameter, WriterDelegate<TParameter> write)
+    {
+        writer.WriteBool(HasValue);
+        if (!HasValue)
+        {
+            return;
+        }
+
+        if (Value is null)
+        {
+            throw new InvalidOperationException("Optional value is marked as present but Value is null.");
+        }
+
+        write(ref writer, Value, parameter);
     }
 }
 
