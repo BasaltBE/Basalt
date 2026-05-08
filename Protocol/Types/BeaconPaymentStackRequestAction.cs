aac3ee3df542f979ef38b078c3acb6fe37a21eb0
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-08 17:41:38 +0300

    feat: add BeaconPaymentStackRequestAction class for handling beacon payment stack requests

---
+++ Protocol/Types/BeaconPaymentStackRequestAction.cs
@@ -0,0 +1,23 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class BeaconPaymentStackRequestAction : IStackRequestAction, DataType
+{
+    public byte ActionType => 10;
+    public int PrimaryEffect { get; set; }
+    public int SecondaryEffect { get; set; }
+
+    public void Read(ref BinaryReader reader)
+    {
+        PrimaryEffect = reader.ReadZigZag();
+        SecondaryEffect = reader.ReadZigZag();
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteZigZag(PrimaryEffect);
+        writer.WriteZigZag(SecondaryEffect);
+    }
+}
