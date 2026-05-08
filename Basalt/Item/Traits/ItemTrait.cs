5ffd9d38899397994c12a4c06d1a063e4ad1c063
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-08 16:37:10 +0300

    feat: expand ItemTrait class with static properties for Types, Tags, Component, and Components

--- Basalt/Item/Traits/ItemTrait.cs
+++ Basalt/Item/Traits/ItemTrait.cs
@@ -1,11 +1,41 @@
+using System.Reflection;
+
 namespace Basalt.Item.Traits;
 
 public abstract class ItemTrait
 {
+    public static readonly string[] Types = [];
+    public static readonly string[] Tags = [];
+    public static readonly Type? Component = null;
+    public static readonly Type[] Components = [];
+
     protected ItemStack ItemStack { get; }
+    public virtual string Identifier
+    {
+        get
+        {
+            if (GetType().GetProperty("Identifier", BindingFlags.Public | BindingFlags.Static) is PropertyInfo property &&
+                property.PropertyType == typeof(string) &&
+                property.GetValue(null) is string identifier &&
+                !string.IsNullOrWhiteSpace(identifier))
+            {
+                return identifier;
+            }
+
+            return GetType().FullName ?? GetType().Name;
+        }
+    }
 
     protected ItemTrait(ItemStack itemStack)
     {
         ItemStack = itemStack;
+    }
+
+    public virtual void OnAdd()
+    {
+    }
+
+    public virtual void OnRemove()
+    {
     }
 }
