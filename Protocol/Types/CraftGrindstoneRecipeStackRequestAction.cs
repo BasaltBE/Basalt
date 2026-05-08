895a46a95f155cb8eb75cb4f02993f53972de59c
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-08 17:41:24 +0300

    feat: add CraftGrindstoneRecipeStackRequestAction class for handling grindstone recipe stack requests

---
+++ Protocol/Types/CraftGrindstoneRecipeStackRequestAction.cs
@@ -0,0 +1,26 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class CraftGrindstoneRecipeStackRequestAction : IStackRequestAction, DataType
+{
+    public byte ActionType => 16;
+    public uint RecipeNetworkId { get; set; }
+    public byte NumberOfCrafts { get; set; }
+    public int Cost { get; set; }
+
+    public void Read(ref BinaryReader reader)
+    {
+        RecipeNetworkId = reader.ReadVarUInt();
+        NumberOfCrafts = reader.ReadUInt8();
+        Cost = reader.ReadZigZag();
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteVarUInt(RecipeNetworkId);
+        writer.WriteUInt8(NumberOfCrafts);
+        writer.WriteZigZag(Cost);
+    }
+}
