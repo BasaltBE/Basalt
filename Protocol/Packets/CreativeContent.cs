d02718306a69a9269ac4eb6ecfc16841c626420d
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-08 16:39:15 +0300

    feat: add CreativeContentPacket for managing creative content data serialization and deserialization

---
+++ Protocol/Packets/CreativeContent.cs
@@ -0,0 +1,50 @@
+using Basalt.Protocol.Enums;
+using Basalt.Protocol.Types;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Packets;
+
+public sealed record CreativeContentPacket : DataPacket
+{
+    public List<CreativeGroup> Groups { get; set; } = [];
+    public List<CreativeItem> Items { get; set; } = [];
+
+    public override PacketId PacketId => PacketId.CreativeContent;
+
+    public override void Deserialize(ref BinaryReader reader)
+    {
+        int groupCount = checked((int)reader.ReadVarUInt());
+        Groups = new List<CreativeGroup>(groupCount);
+        for (int i = 0; i < groupCount; i++)
+        {
+            CreativeGroup group = new();
+            group.Read(ref reader);
+            Groups.Add(group);
+        }
+
+        int itemCount = checked((int)reader.ReadVarUInt());
+        Items = new List<CreativeItem>(itemCount);
+        for (int i = 0; i < itemCount; i++)
+        {
+            CreativeItem item = new();
+            item.Read(ref reader);
+            Items.Add(item);
+        }
+    }
+
+    public override void Serialize(ref BinaryWriter writer)
+    {
+        writer.WriteVarUInt((uint)Groups.Count);
+        for (int i = 0; i < Groups.Count; i++)
+        {
+            Groups[i].Write(ref writer);
+        }
+
+        writer.WriteVarUInt((uint)Items.Count);
+        for (int i = 0; i < Items.Count; i++)
+        {
+            Items[i].Write(ref writer);
+        }
+    }
+}
