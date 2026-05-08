8ab551c0ae8b6a38706cca164e1ab31b9c3143f0
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-08 19:09:36 +0300

    feat: refactor InteractPacket serialization and deserialization for improved clarity and error handling

--- Protocol/Packets/Interact.cs
+++ Protocol/Packets/Interact.cs
@@ -16,19 +16,36 @@ public sealed record InteractPacket : DataPacket
     public override void Deserialize(ref BinaryReader reader)
     {
         ActionType = (InteractActionType)reader.ReadUInt8();
-        TargetEntityRuntimeId = reader.ReadVarULong();
-        Position.Read(ref reader, static (ref BinaryReader r) =>
+        if (reader.Remaining > 0)
+        {
+            TargetEntityRuntimeId = reader.ReadVarULong();
+        }
+        else
+        {
+            TargetEntityRuntimeId = 0;
+        }
+
+        if (ActionType == InteractActionType.MouseOverEntity && reader.Remaining >= 12)
         {
             Vec3f value = new();
-            value.Read(ref r);
-            return value;
-        });
+            value.Read(ref reader);
+            Position = new OptionalValue<Vec3f> { HasValue = true, Value = value };
+        }
+        else
+        {
+            Position = new OptionalValue<Vec3f> { HasValue = false };
+        }
     }
 
     public override void Serialize(ref BinaryWriter writer)
     {
         writer.WriteUInt8((byte)ActionType);
+
         writer.WriteVarULong(TargetEntityRuntimeId);
-        Position.Write(ref writer, static (ref BinaryWriter w, Vec3f value) => value.Write(ref w));
+
+        if (ActionType == InteractActionType.MouseOverEntity && Position.HasValue && Position.Value is { } value)
+        {
+            value.Write(ref writer);
+        }
     }
 }
