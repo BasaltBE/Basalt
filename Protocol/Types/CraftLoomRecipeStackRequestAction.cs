1ac0c2c61a44a500884c29e1ba06360772a599cb
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-08 16:47:19 +0300

    feat: add CraftLoomRecipeStackRequestAction class for handling loom recipe stack requests

---
+++ Protocol/Types/CraftLoomRecipeStackRequestAction.cs
@@ -0,0 +1,23 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public sealed class CraftLoomRecipeStackRequestAction : IStackRequestAction, DataType
+{
+    public byte ActionType => 17;
+    public string Pattern { get; set; } = string.Empty;
+    public byte TimesCrafted { get; set; }
+
+    public void Read(ref BinaryReader reader)
+    {
+        Pattern = reader.ReadVarString();
+        TimesCrafted = reader.ReadUInt8();
+    }
+
+    public void Write(ref BinaryWriter writer)
+    {
+        writer.WriteVarString(Pattern);
+        writer.WriteUInt8(TimesCrafted);
+    }
+}
