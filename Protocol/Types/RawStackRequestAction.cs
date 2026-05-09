f2c7e951863e5a87995496f6a60a213192deca1d
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-09 20:34:32 +0300

    Update RawStackRequestAction.cs

--- Protocol/Types/RawStackRequestAction.cs
+++ Protocol/Types/RawStackRequestAction.cs
@@ -8,6 +8,6 @@ public sealed class RawStackRequestAction : IStackRequestAction, DataType
     public byte Type { get; set; }
     public byte[] Data { get; set; } = [];
     public byte ActionType => Type;
-    public void Read(ref BinaryReader reader) => throw new InvalidOperationException();
+    public void Read(ref BinaryReader reader) { }
     public void Write(ref BinaryWriter writer) => writer.WriteBytes(Data);
 }
