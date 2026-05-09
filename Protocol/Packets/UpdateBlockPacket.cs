4501c401c3cdefa260dc9150d15ca64ee02ed933
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-09 20:34:35 +0300

    Implement UpdateBlockPacket.cs

---
+++ Protocol/Packets/UpdateBlockPacket.cs
@@ -0,0 +1,34 @@
+using Basalt.Protocol.Enums;
+using Basalt.Protocol.Types;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Packets;
+
+public sealed record UpdateBlockPacket : DataPacket
+{
+    public BlockPos Position { get; set; }
+    public uint NetworkBlockId { get; set; }
+    public UpdateBlockFlagsType Flags { get; set; }
+    public UpdateBlockLayerType Layer { get; set; }
+
+    public override PacketId PacketId => PacketId.UpdateBlock;
+
+    public override void Deserialize(ref BinaryReader reader)
+    {
+        BlockPos position = Position;
+        position.Read(ref reader);
+        Position = position;
+        NetworkBlockId = reader.ReadVarUInt();
+        Flags = (UpdateBlockFlagsType)reader.ReadVarUInt();
+        Layer = (UpdateBlockLayerType)reader.ReadVarUInt();
+    }
+
+    public override void Serialize(ref BinaryWriter writer)
+    {
+        Position.Write(ref writer);
+        writer.WriteVarUInt(NetworkBlockId);
+        writer.WriteVarUInt((uint)Flags);
+        writer.WriteVarUInt((uint)Layer);
+    }
+}
