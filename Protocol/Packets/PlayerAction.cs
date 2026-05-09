fe702a3374da6051f674f38993ffd67d877d926e
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-09 20:34:34 +0300

    Implement PlayerAction.cs

---
+++ Protocol/Packets/PlayerAction.cs
@@ -0,0 +1,39 @@
+using Basalt.Protocol.Enums;
+using Basalt.Protocol.Types;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Packets;
+
+public sealed record PlayerActionPacket : DataPacket
+{
+    public ulong EntityRuntimeId { get; set; }
+    public int ActionType { get; set; }
+    public BlockPos BlockPosition { get; set; }
+    public BlockPos ResultPosition { get; set; }
+    public int BlockFace { get; set; }
+
+    public override PacketId PacketId => PacketId.PlayerAction;
+
+    public override void Deserialize(ref BinaryReader reader)
+    {
+        EntityRuntimeId = reader.ReadVarULong();
+        ActionType = reader.ReadVarInt();
+        BlockPos blockPosition = BlockPosition;
+        blockPosition.Read(ref reader);
+        BlockPosition = blockPosition;
+        BlockPos resultPosition = ResultPosition;
+        resultPosition.Read(ref reader);
+        ResultPosition = resultPosition;
+        BlockFace = reader.ReadVarInt();
+    }
+
+    public override void Serialize(ref BinaryWriter writer)
+    {
+        writer.WriteVarULong(EntityRuntimeId);
+        writer.WriteVarInt(ActionType);
+        BlockPosition.Write(ref writer);
+        ResultPosition.Write(ref writer);
+        writer.WriteVarInt(BlockFace);
+    }
+}
