f006f0b48872b34f0d1c0a18afada6157810bcd5
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-09 20:34:34 +0300

    Implement LevelSoundEventPacket.cs

---
+++ Protocol/Packets/LevelSoundEventPacket.cs
@@ -0,0 +1,46 @@
+using Basalt.Protocol.Enums;
+using Basalt.Protocol.Types;
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Packets;
+
+public sealed record LevelSoundEventPacket : DataPacket
+{
+    public LevelSoundEvent Event { get; set; }
+    public Vec3f Position { get; set; }
+    public int Data { get; set; }
+    public string ActorIdentifier { get; set; } = string.Empty;
+    public bool IsBabyMob { get; set; }
+    public bool IsGlobal { get; set; }
+    public long UniqueActorId { get; set; }
+    public Optional<Vec3f> FireAtPosition { get; set; } = new();
+
+    public override PacketId PacketId => PacketId.LevelSoundEvent;
+
+    public override void Deserialize(ref BinaryReader reader)
+    {
+        Event = (LevelSoundEvent)reader.ReadVarUInt();
+        Vec3f position = Position;
+        position.Read(ref reader);
+        Position = position;
+        Data = reader.ReadVarInt(); // ZigZag
+        ActorIdentifier = reader.ReadVarString();
+        IsBabyMob = reader.ReadBool();
+        IsGlobal = reader.ReadBool();
+        UniqueActorId = reader.ReadInt64(true);
+        FireAtPosition.Read(ref reader);
+    }
+
+    public override void Serialize(ref BinaryWriter writer)
+    {
+        writer.WriteVarUInt((uint)Event);
+        Position.Write(ref writer);
+        writer.WriteZigZag(Data);
+        writer.WriteVarString(ActorIdentifier);
+        writer.WriteBool(IsBabyMob);
+        writer.WriteBool(IsGlobal);
+        writer.WriteInt64(UniqueActorId, true);
+        FireAtPosition.Write(ref writer);
+    }
+}
