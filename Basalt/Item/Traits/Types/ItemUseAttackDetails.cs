fd20d3dd5e8967d9b7d051cf561fd7fd134b5e5a
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-10 15:53:41 +0300

    Add ItemUseAttackDetails struct for item use attack details

---
+++ Basalt/Item/Traits/Types/ItemUseAttackDetails.cs
@@ -0,0 +1,6 @@
+using Basalt.Core;
+using Basalt.Protocol.Types;
+
+namespace Basalt.Item.Traits.Types;
+
+public readonly record struct ItemUseAttackDetails(Player Player, Basalt.Entity.Entity Target, int HotBarSlot, Vec3f Position, Vec3f ClickedPosition);
