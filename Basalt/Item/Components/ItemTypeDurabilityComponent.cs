002ba0b4aa1c5dd6547aed6a3704d7ad7d691627
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-10 15:52:20 +0300

    Add ItemTypeDurabilityComponent for managing item durability and damage chances

---
+++ Basalt/Item/Components/ItemTypeDurabilityComponent.cs
@@ -0,0 +1,30 @@
+using Basalt.Protocol.Nbt;
+
+namespace Basalt.Item.Components;
+
+public sealed class ItemTypeDurabilityComponent : ItemTypeComponent
+{
+    public new static string Identifier => "minecraft:durability";
+
+    public ItemTypeDurabilityComponent(ItemType type, CompoundTag component) : base(type, component)
+    {
+    }
+
+    public int GetMaxDurability()
+    {
+        return Component.Get<IntTag>("max_durability")?.Value ?? 0;
+    }
+
+    public (int Min, int Max) GetDamageChance()
+    {
+        CompoundTag? chance = Component.Get<CompoundTag>("damage_chance");
+        if (chance is null)
+        {
+            return (0, 0);
+        }
+
+        int min = chance.Get<IntTag>("min")?.Value ?? 0;
+        int max = chance.Get<IntTag>("max")?.Value ?? 0;
+        return (min, max);
+    }
+}
