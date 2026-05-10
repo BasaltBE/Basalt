c39495fd2ae67296480b6411e9f81bff4a919a0f
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-10 15:37:25 +0300

    Add player data loading and saving methods to WorldProvider

--- Basalt/World/Dimension/Provider/WorldProvider.cs
+++ Basalt/World/Dimension/Provider/WorldProvider.cs
@@ -1,4 +1,5 @@
 using Basalt.Protocol.Enums;
+using Basalt.Protocol.Nbt;
 using ChunkColumn = Basalt.World.Dimension.Chunk.Chunk;
 
 namespace Basalt.World.Dimension.Provider;
@@ -10,6 +11,15 @@ public abstract class WorldProvider : IDisposable
     public abstract ChunkColumn? LoadChunk(DimensionType dimensionType, int x, int z);
     public abstract void SaveChunk(ChunkColumn chunk);
     public abstract void DeleteChunk(int x, int z);
+    public virtual CompoundTag? LoadPlayerData(string xuid)
+    {
+        return null;
+    }
+
+    public virtual void SavePlayerData(string xuid, CompoundTag data)
+    {
+    }
+
     public abstract void Dispose();
 
     protected static long HashChunk(int x, int z)
