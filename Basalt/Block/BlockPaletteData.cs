195c2891ead7cd3067d5216f197a2451c3efcac0
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-10 15:40:52 +0300

    Add BlockMetadataData class and update serialization context

--- Basalt/Block/BlockPaletteData.cs
+++ Basalt/Block/BlockPaletteData.cs
@@ -20,9 +20,19 @@ public sealed class BlockPermutationData
     public Dictionary<string, object> State { get; set; } = [];
 }
 
+public sealed class BlockMetadataData
+{
+    [JsonPropertyName("identifier")]
+    public string Identifier { get; set; } = string.Empty;
+
+    [JsonPropertyName("hardness")]
+    public float Hardness { get; set; }
+}
+
 [JsonSourceGenerationOptions(PropertyNameCaseInsensitive = false)]
 [JsonSerializable(typeof(List<BlockTypeData>))]
 [JsonSerializable(typeof(List<BlockPermutationData>))]
+[JsonSerializable(typeof(List<BlockMetadataData>))]
 internal partial class BlockPaletteJsonContext : JsonSerializerContext
 {
 }
