b332772cf2352c0c3256f0d9ef5b4fda54146d78
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-13 00:32:11 +0300

    Enhance Dimension class with block actor management and improve broadcasting logic

--- Basalt/World/Dimension/Dimension.cs
+++ Basalt/World/Dimension/Dimension.cs
@@ -1,6 +1,9 @@
 using Basalt.Block;
+using Basalt.Block.Traits;
 using Basalt.Protocol.Packets;
 using Basalt.Protocol.Enums;
+using Basalt.Protocol.Nbt;
+using Basalt.Protocol.Types;
 using Basalt.World.Dimension.Generation;
 using Basalt.World.Dimension.Provider;
 // Damn name spacing
@@ -21,8 +24,7 @@ public sealed class Dimension : IDisposable
     public Difficulty Difficulty { get; set; } = Difficulty.Normal;
     public global::Basalt.World.World? World { get; internal set; }
     public global::Basalt.World.DimensionGameRules Gamerules { get; } = new();
-    internal Action<DataPacket>? PacketBroadcaster { get; set; }
-    internal Action<DataPacket, global::Basalt.Entity.Entity>? PacketBroadcasterExcept { get; set; }
+    internal Action<DataPacket, BroadcastOptions>? PacketBroadcaster { get; set; }
 
     public Dimension(string identifier, DimensionType type, WorldProvider provider, Generator? generator = null)
     {
@@ -106,6 +108,31 @@ public sealed class Dimension : IDisposable
 
     public void SaveDirtyChunks()
     {
+        foreach (ChunkColumn loadedChunk in _chunks.Values)
+        {
+            foreach (KeyValuePair<(int X, int Y, int Z), global::Basalt.Block.Block> actorEntry in loadedChunk.GetAllBlockActors())
+            {
+                BlockPos position = new()
+                {
+                    X = actorEntry.Key.X,
+                    Y = actorEntry.Key.Y,
+                    Z = actorEntry.Key.Z
+                };
+
+                BlockLevelStorage? storage = loadedChunk.GetBlockStorage(position);
+                if (storage is null)
+                {
+                    storage = new BlockLevelStorage(loadedChunk);
+                    storage.SetPosition(position);
+                    storage.Set("id", new StringTag { Name = "id", Value = GetBlockActorId(actorEntry.Value.Type.Identifier) });
+                    storage.Set("isMovable", new ByteTag { Name = "isMovable", Value = 1 });
+                }
+
+                actorEntry.Value.WriteTraits(storage);
+                loadedChunk.SetBlockStorage(position, storage, dirty: true);
+            }
+        }
+
         foreach (ChunkColumn chunk in _chunks.Values)
         {
             if (!chunk.Dirty)
@@ -226,6 +253,92 @@ public sealed class Dimension : IDisposable
     {
         ChunkColumn chunk = GetOrCreateChunk(x >> 4, z >> 4);
         chunk.SetPermutation(x & 0xF, y, z & 0xF, permutation, layer, dirty);
+
+        BlockPos position = new() { X = x, Y = y, Z = z };
+        if (permutation.Type.Traits.Count > 0)
+        {
+            global::Basalt.Block.Block? block = chunk.GetBlockActor(position);
+            if (block is null)
+            {
+                block = new global::Basalt.Block.Block(permutation);
+                chunk.SetBlockActor(position, block);
+            }
+            else
+            {
+                block.SetPermutation(permutation);
+            }
+
+            BlockLevelStorage? storage = chunk.GetBlockStorage(position);
+            if (storage is null)
+            {
+                storage = new BlockLevelStorage(chunk);
+                storage.SetPosition(position);
+                storage.Set("id", new StringTag { Name = "id", Value = GetBlockActorId(permutation.Type.Identifier) });
+                storage.Set("isMovable", new ByteTag { Name = "isMovable", Value = 1 });
+            }
+            chunk.SetBlockStorage(position, storage, dirty);
+        }
+        else
+        {
+            chunk.SetBlockActor(position, null);
+            chunk.SetBlockStorage(position, null, dirty);
+        }
+    }
+
+    public global::Basalt.Block.Block? GetBlock(int x, int y, int z)
+    {
+        ChunkColumn? chunk = GetChunk(x >> 4, z >> 4);
+        if (chunk is null)
+        {
+            return null;
+        }
+
+        BlockPos position = new() { X = x, Y = y, Z = z };
+        global::Basalt.Block.Block? block = chunk.GetBlockActor(position);
+        if (block is not null)
+        {
+            return block;
+        }
+
+        BlockPermutation perm = chunk.GetPermutation(x & 0xF, y, z & 0xF);
+        if (perm.Type.Traits.Count > 0)
+        {
+            block = new global::Basalt.Block.Block(perm);
+            BlockLevelStorage? storage = chunk.GetBlockStorage(position);
+            if (storage is not null)
+            {
+                block.ReadTraits(storage);
+            }
+
+            ChestTrait? chestTrait = block.GetTrait<ChestTrait>();
+            if (chestTrait?.Container is not null)
+            {
+                chestTrait.Container.Dimension = this;
+                chestTrait.Container.Position = new BlockPos { X = x, Y = y, Z = z };
+            }
+
+            chunk.SetBlockActor(position, block);
+            return block;
+        }
+
+        return null;
+    }
+
+    public void SetBlock(int x, int y, int z, global::Basalt.Block.Block block)
+    {
+        ChunkColumn chunk = GetOrCreateChunk(x >> 4, z >> 4);
+        chunk.SetBlockActor(new BlockPos { X = x, Y = y, Z = z }, block);
+    }
+
+    public void RemoveBlock(int x, int y, int z)
+    {
+        ChunkColumn? chunk = GetChunk(x >> 4, z >> 4);
+        if (chunk is null)
+        {
+            return;
+        }
+
+        chunk.SetBlockActor(new BlockPos { X = x, Y = y, Z = z }, null);
     }
 
     public int GetBiome(int x, int y, int z)
@@ -265,18 +378,15 @@ public sealed class Dimension : IDisposable
                 continue;
             }
 
-            entity.TickTraits(currentTick, deltaTick);
+            entity.Tick(currentTick, deltaTick);
         }
     }
 
-    public void Broadcast(DataPacket packet)
+    public void Broadcast(DataPacket packet, BroadcastOptions? options = null)
     {
-        PacketBroadcaster?.Invoke(packet);
-    }
-
-    public void BroadcastExcept(DataPacket packet, global::Basalt.Entity.Entity exclude)
-    {
-        PacketBroadcasterExcept?.Invoke(packet, exclude);
+        BroadcastOptions resolved = options ?? new BroadcastOptions();
+        resolved.Center ??= GetPacketPosition(packet);
+        PacketBroadcaster?.Invoke(packet, resolved);
     }
 
     internal void AddEntity(global::Basalt.Entity.Entity entity)
@@ -292,5 +402,49 @@ public sealed class Dimension : IDisposable
     private static long HashChunk(int x, int z)
     {
         return ((long)x << 32) | (uint)z;
+    }
+
+    private static string GetBlockActorId(string blockIdentifier)
+    {
+        return blockIdentifier switch
+        {
+            "minecraft:chest" => "Chest",
+            "minecraft:trapped_chest" => "Chest",
+            _ => blockIdentifier
+        };
+    }
+
+    private static Vec3f? GetPacketPosition(DataPacket packet)
+    {
+        switch (packet)
+        {
+            case UpdateBlockPacket updateBlock:
+                return new Vec3f
+                {
+                    X = updateBlock.Position.X,
+                    Y = updateBlock.Position.Y,
+                    Z = updateBlock.Position.Z
+                };
+
+            case BlockActorDataPacket blockActor:
+                return new Vec3f
+                {
+                    X = blockActor.Position.X,
+                    Y = blockActor.Position.Y,
+                    Z = blockActor.Position.Z
+                };
+
+            case LevelEventPacket levelEvent:
+                return levelEvent.Position;
+
+            case LevelSoundEventPacket levelSoundEvent:
+                return levelSoundEvent.Position;
+
+            case MovePlayerPacket movePlayer:
+                return movePlayer.Position;
+
+            default:
+                return null;
+        }
     }
 }
