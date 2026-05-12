f026dcaaccd108380642cbb92e3ab27d0b0c7e05
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-12 20:14:53 +0300

    Refactor RaknetServerOptions to use record struct for improved initialization and readability

--- RakNet/ServerOptions.cs
+++ RakNet/ServerOptions.cs
@@ -1,17 +1,8 @@
 namespace Basalt.RakNet;
 
-public readonly record struct RaknetServerOptions
-{
-    public RaknetServerOptions()
-    {
-        MaxMtu = 1400;
-        MaxConnections = 255;
-        Advertisement = "MCPE;Basalt;924;1.21.90;0;10;03124212345;Bedrock level;Survival;1;19132;19133;";
-        EnableCookies = true;
-    }
-
-    public ushort MaxMtu { get; init; }
-    public int MaxConnections { get; init; }
-    public string Advertisement { get; init; }
-    public bool EnableCookies { get; init; }
-}
+public readonly record struct RaknetServerOptions(
+    ushort MaxMtu = 1400,
+    int MaxConnections = 255,
+    string Advertisement = "MCPE;Basalt;924;1.21.90;0;10;03124212345;Bedrock level;Survival;1;19132;19133;",
+    bool EnableCookies = true
+);
