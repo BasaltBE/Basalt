9a12add5f34c3bbe6389875a2ecc09309e7c7c79
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-16 16:14:19 +0300

    Refactor WorldProvider to include DimensionType in chunk-related methods for improved clarity

--- Basalt/World/Dimension/Provider/WorldProvider.cs
+++ Basalt/World/Dimension/Provider/WorldProvider.cs
@@ -7,10 +7,10 @@ namespace Basalt.World.Dimension.Provider;
 public abstract class WorldProvider : IDisposable
 {
     public abstract string Identifier { get; }
-    public abstract bool HasChunk(int x, int z);
+    public abstract bool HasChunk(DimensionType dimensionType, int x, int z);
     public abstract ChunkColumn? LoadChunk(DimensionType dimensionType, int x, int z);
     public abstract void SaveChunk(ChunkColumn chunk);
-    public abstract void DeleteChunk(int x, int z);
+    public abstract void DeleteChunk(DimensionType dimensionType, int x, int z);
     public virtual CompoundTag? LoadPlayerData(string xuid)
     {
         return null;
