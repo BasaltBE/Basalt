cab1e3a81f25566915702eebe9ec794361389dc5
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-21 14:51:24 +0300

    Reimplement BoolType

---
+++ Protocol/Types/BoolType.cs
@@ -0,0 +1,15 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public struct BoolType : DataType
+{
+    public bool Value { get; set; }
+
+    public void Read(BinaryReader reader) => Value = reader.ReadBool();
+    public void Write(BinaryWriter writer) => writer.WriteBool(Value);
+
+    public static implicit operator bool(BoolType value) => value.Value;
+    public static implicit operator BoolType(bool value) => new() { Value = value };
+}
