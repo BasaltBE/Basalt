e7793751733a7a0aa81087a061b50a86d05c4f5f
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-21 15:25:42 +0300

    Reimplement SetLocalPlayerAsInitialized

---
+++ Protocol/Packets/SetLocalPlayerAsInitialized.cs
@@ -0,0 +1,23 @@
+using Basalt.Protocol.Enums;
+using Basalt.Protocol.Packets;
+
+namespace Basalt.Protocol.Packets;
+
+[Packet(PacketId.SetLocalPlayerAsInitialized)]
+public sealed record SetLocalPlayerAsInitializedPacket : DataPacket
+{   
+    /// <summary>
+    /// The runtime id of the entity
+    /// </summary>
+    public ulong EntityRuntimeId;
+
+    public override void Deserialize(Binary.BinaryReader reader)
+    {
+        EntityRuntimeId = reader.ReadVarULong();
+    }
+
+    public override void Serialize(Binary.BinaryWriter writer)
+    {
+        writer.WriteVarULong(EntityRuntimeId);
+    }
+}
