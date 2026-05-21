bfcb6e445f8442fb81b6c0c9fbf4850433858b93
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-21 17:45:34 +0300

    Reimplement Vec2f

---
+++ Protocol/Types/Vec2f.cs
@@ -0,0 +1,29 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public struct Vec2f : DataType
+{
+    /// <summary>
+    /// X coordinate of the vector.
+    /// </summary>
+    public float X { get; set; }
+
+    /// <summary>
+    /// Y coordinate of the vector.
+    /// </summary>
+    public float Y { get; set; }
+
+    public void Read(BinaryReader reader)
+    {
+        X = reader.ReadF32(true);
+        Y = reader.ReadF32(true);
+    }
+
+    public void Write(BinaryWriter writer)
+    {
+        writer.WriteF32(X, true);
+        writer.WriteF32(Y, true);
+    }
+}
