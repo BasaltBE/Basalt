cd316f11121267d1b2338ccbf60eb1e03cd56008
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-21 14:49:11 +0300

    Reimplement Vec3f

--- Protocol/Types/Vec3f.cs
+++ Protocol/Types/Vec3f.cs
@@ -4,9 +4,20 @@ using BinaryWriter = Basalt.Binary.BinaryWriter;
 namespace Basalt.Protocol.Types;
 
 public struct Vec3f : DataType
-{
+{   
+    /// <summary>
+    /// X coordinate of the vector.
+    /// </summary>
     public float X { get; set; }
+
+    /// <summary>
+    /// Y coordinate of the vector.
+    /// </summary>
     public float Y { get; set; }
+    
+    /// <summary>
+    /// Z coordinate of the vector.
+    /// </summary>
     public float Z { get; set; }
 
     public void Read(BinaryReader reader)
