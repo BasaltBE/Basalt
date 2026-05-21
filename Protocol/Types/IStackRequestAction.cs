d598827de5acea6fe0fad6eaaa15b0418f708c07
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-21 19:43:56 +0300

    Reimplement many more packets

---
+++ Protocol/Types/IStackRequestAction.cs
@@ -0,0 +1,11 @@
+using BinaryReader = Basalt.Binary.BinaryReader;
+using BinaryWriter = Basalt.Binary.BinaryWriter;
+
+namespace Basalt.Protocol.Types;
+
+public interface IStackRequestAction
+{
+    byte ActionType { get; }
+    void Read(BinaryReader reader);
+    void Write(BinaryWriter writer);
+}
