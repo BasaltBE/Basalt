9627eb8debfcd326e944b47eda7e19e9b812f425
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-26 20:26:10 +0300

    Implement ActorPickRequestPacket

---
+++ Protocol/Packets/ActorPickRequest.cs
@@ -0,0 +1,36 @@
+using Basalt.Protocol.Enums;
+
+namespace Basalt.Protocol.Packets;
+
+[Packet(PacketId.ActorPickRequest)]
+public sealed record ActorPickRequestPacket : DataPacket
+{
+    /// <summary>
+    /// Unique id of the picked actor.
+    /// </summary>
+    public long EntityUniqueId;
+
+    /// <summary>
+    /// Hotbar slot used for the pick request.
+    /// </summary>
+    public byte HotBarSlot;
+
+    /// <summary>
+    /// Whether metadata is requested.
+    /// </summary>
+    public bool WithData;
+
+    public override void Deserialize(Binary.BinaryReader reader)
+    {
+        EntityUniqueId = reader.ReadInt64(true);
+        HotBarSlot = reader.ReadUInt8();
+        WithData = reader.ReadBool();
+    }
+
+    public override void Serialize(Binary.BinaryWriter writer)
+    {
+        writer.WriteInt64(EntityUniqueId, true);
+        writer.WriteUInt8(HotBarSlot);
+        writer.WriteBool(WithData);
+    }
+}
