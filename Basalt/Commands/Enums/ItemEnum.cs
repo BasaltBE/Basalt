b30e0396ea7d9ce63d2dae27e47252ee5f120b1d
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-05-26 20:26:15 +0300

    e

--- Basalt/Commands/Enums/ItemEnum.cs
+++ Basalt/Commands/Enums/ItemEnum.cs
@@ -4,15 +4,31 @@ namespace Basalt.Commands;
 
 public class ItemEnum : CommandEnum
 {
+    const string VanillaPrefix = "minecraft:";
+
     public string Raw = string.Empty;
 
     public ItemType Type = ItemType.Air;
 
-    public ItemEnum() : base("Item") { }
+    public ItemEnum() : base("Item")
+    {
+        ItemPalette.LoadVanilla();
+        Options = [.. ItemType.Types.Keys.Select(TrimPrefix)];
+    }
 
     public ItemEnum(string raw, ItemType type) : base("Item")
     {
         Raw = raw;
         Type = type;
+    }
+
+    static string TrimPrefix(string identifier)
+    {
+        if (!identifier.StartsWith(VanillaPrefix, StringComparison.Ordinal))
+        {
+            return identifier;
+        }
+
+        return identifier[VanillaPrefix.Length..];
     }
 }
