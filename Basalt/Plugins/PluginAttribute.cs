a301a34c484093b9c613a8b99836656171f1b3ee
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-06-03 23:00:00 +0300

    PluginAttribute

---
+++ Basalt/Plugins/PluginAttribute.cs
@@ -0,0 +1,15 @@
+namespace Basalt.Server.Plugins;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public sealed class PluginAttribute : Attribute
+{
+    public string Name;
+    public string Version;
+    public string[] Authors = [];
+
+    public PluginAttribute(string name, string version)
+    {
+        Name = name;
+        Version = version;
+    }
+}
