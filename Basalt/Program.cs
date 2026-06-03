5a1f93e9326174f97c7eced54175b29977aea7af
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-06-03 22:58:26 +0300

    plugins-directory

--- Basalt/Program.cs
+++ Basalt/Program.cs
@@ -43,6 +43,7 @@ namespace Basalt.Server
             if (!props.HasProperty("default-world")) props.SetStringProperty("default-world", "world");
             if (!props.HasProperty("world-provider")) props.SetStringProperty("world-provider", "leveldb");
             if (!props.HasProperty("world-path")) props.SetStringProperty("world-path", Path.Combine("worlds", props.GetStringProperty("default-world", "world") ?? "world"));
+            if (!props.HasProperty("plugins-directory")) props.SetStringProperty("plugins-directory", "plugins");
             if (!props.HasProperty("compression-threshold")) props.SetNumericalProperty("compression-threshold", 1);
             if (!props.HasProperty("compression-algorithm")) props.SetStringProperty("compression-algorithm", "zlib");
         }
