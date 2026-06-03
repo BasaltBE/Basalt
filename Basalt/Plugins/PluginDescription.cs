73d9cd10b443f442a4f8ab6afc71439de70c4674
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-06-03 22:58:02 +0300

    PluginDescription, for plugin descriptors

---
+++ Basalt/Plugins/PluginDescription.cs
@@ -0,0 +1,18 @@
+namespace Basalt.Server.Plugins;
+
+public sealed class PluginDescription
+{
+    public string Name = string.Empty;
+    public string Version = string.Empty;
+    public string[] Authors = [];
+
+    public static PluginDescription From(PluginAttribute attribute)
+    {
+        return new PluginDescription
+        {
+            Name = attribute.Name,
+            Version = attribute.Version,
+            Authors = attribute.Authors
+        };
+    }
+}
