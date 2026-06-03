31e86330f8302bfb29b68fab767d66ebb3db01ca
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-06-03 13:32:57 +0300

    Update the EntityPaletteData.cs to accept new data

--- Basalt/Entity/EntityPaletteData.cs
+++ Basalt/Entity/EntityPaletteData.cs
@@ -1,5 +1,6 @@
 namespace Basalt.Server.Entity;
 
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 public sealed class EntityTypeData
@@ -9,6 +10,27 @@ public sealed class EntityTypeData
 
     [JsonPropertyName("components")]
     public List<string> Components { get; set; } = [];
+
+    [JsonPropertyName("minecraft:loot")]
+    public EntityLootData? Loot { get; set; }
+
+    [JsonPropertyName("propertiesPayload")]
+    public EntityPropertiesPayloadData? PropertiesPayload { get; set; }
+}
+
+public sealed class EntityLootData
+{
+    [JsonPropertyName("table")]
+    public string Table { get; set; } = string.Empty;
+}
+
+public sealed class EntityPropertiesPayloadData
+{
+    [JsonPropertyName("components")]
+    public Dictionary<string, JsonElement> Components { get; set; } = [];
+
+    [JsonPropertyName("componentGroups")]
+    public Dictionary<string, Dictionary<string, JsonElement>> ComponentGroups { get; set; } = [];
 }
 
 [JsonSourceGenerationOptions(PropertyNameCaseInsensitive = false)]
