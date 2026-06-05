9d0300f7924a63c2c77ab86b347ffe932b0a3be3
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-06-05 22:27:59 +0300

    ModalFormResponse handler

---
+++ Basalt/Network/Handlers/ModalFormResponse.cs
@@ -0,0 +1,28 @@
+namespace Basalt.Core.Network.Handlers;
+
+using Basalt.Protocol.Packets;
+using Basalt.RakNet;
+
+public static class ModalFormResponse
+{
+    public static void Handle(Server server, NetworkConnection connection, ReadOnlySpan<byte> packetBuffer)
+    {
+        ModalFormResponsePacket packet = new();
+        int offset = 0;
+        Binary.BinaryReader reader = new(packetBuffer, ref offset);
+        packet = (ModalFormResponsePacket)Protocol.Io.Packet.Deserialize(reader);
+
+        if (!server.Players.TryGetValue(connection, out Player.Player? player))
+        {
+            connection.Disconnect();
+            return;
+        }
+
+        if (!player.PendingForms.Remove(packet.FormId, out Forms.FormParticipant? participant))
+        {
+            return;
+        }
+
+        participant.Complete(packet.Data, packet.Canceled);
+    }
+}
