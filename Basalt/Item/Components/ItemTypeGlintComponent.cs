371377627ed4bbdd43bcba457b5729f4d8a664f6
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-07-17 21:54:46 +0300

    Implement Glint component for items

---
+++ Basalt/Item/Components/ItemTypeGlintComponent.cs
@@ -0,0 +1,25 @@
+namespace Basalt.Core.Item.Components;
+
+using Basalt.Protocol.Nbt;
+
+
+/// <summary>
+/// Represents the "minecraft:foil" component that controls
+/// whether the item has the enchanted glint render effect.
+/// </summary>
+public sealed class ItemTypeGlintComponent : ItemTypeComponent
+{
+  public new static string Identifier => "minecraft:foil";
+
+  public ItemTypeGlintComponent(ItemType type, CompoundTag component) : base(type, component)
+  {
+  }
+
+  /// <summary>
+  /// Whether the item displays the enchanted glint effect.
+  /// </summary>
+  public bool HasGlint()
+  {
+    return (Component.Get<ByteTag>("value")?.Value ?? 0) != 0;
+  }
+}
