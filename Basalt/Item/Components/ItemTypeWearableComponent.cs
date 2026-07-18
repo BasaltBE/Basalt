4ed8123737ce6344d91e3e015728d279859de68f
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-07-18 19:13:07 +0300

    fix GetSlot in  ItemTypeWearableComponent

--- Basalt/Item/Components/ItemTypeWearableComponent.cs
+++ Basalt/Item/Components/ItemTypeWearableComponent.cs
@@ -12,6 +12,23 @@ public sealed class ItemTypeWearableComponent : ItemTypeComponent
 
     public int GetSlot()
     {
-        return Component.Get<IntTag>("slot")?.Value ?? -1;
+        if (Component.Get<IntTag>("slot") is IntTag intSlot)
+        {
+            return intSlot.Value;
+        }
+
+        if (Component.Get<StringTag>("slot") is StringTag strSlot)
+        {
+            return strSlot.Value switch
+            {
+                "slot.armor.head" => 0,
+                "slot.armor.chest" => 1,
+                "slot.armor.legs" => 2,
+                "slot.armor.feet" => 3,
+                _ => -1
+            };
+        }
+
+        return -1;
     }
 }
